6a121716a34a68909d54cee2cf16826c6c0b3543
Juan_Anzola <juan.anzola.2002@gmail.com>
2024-05-22 13:13:57 -0500

    Fixing login and common redirection

--- lib/presentation/pages/auth/login_page.dart
+++ lib/presentation/pages/auth/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:app_pamii/presentation/pages/auth/widgets/form_login.dart';
+import 'package:app_pamii/presentation/pages/auth/widgets/redirection_page.dart';
 import 'package:app_pamii/presentation/providers/auth/form_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class LoginPage extends ConsumerWidget {
@@ -20,8 +22,8 @@ class LoginPage extends ConsumerWidget {
             fit: BoxFit.cover,
           ),
         ),
-                if (formData.isLoading) const Center(child: CircularProgressIndicator()),
-
+        if (formData.isLoading)
+          const Center(child: CircularProgressIndicator()),
         SafeArea(
           child: Padding(
             padding: const EdgeInsets.all(10.0),
@@ -30,18 +32,29 @@ class LoginPage extends ConsumerWidget {
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 if (formData.isLoading) const CircularProgressIndicator(),
-                const Text(
-                  'Bienvenido',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 50,
-                      fontWeight: FontWeight.bold),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      ' Bienvenido ',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
                 ),
-                const Text(
-                  '¡Es un gusto verte!',
-                  style: TextStyle(color: Colors.white, fontSize: 30),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      ' ¡Es un gusto verte! ',
+                      style: TextStyle(color: Colors.white, fontSize: 30),
+                    ),
+                  ],
                 ),
-                const LoginForm()
+                const Expanded(child: LoginForm()),
+                const PageRedirection(),
               ],
             ),
           ),
---
+++ lib/presentation/pages/auth/widgets/redirection_page.dart
@@ -0,0 +1,32 @@
+import 'package:app_pamii/core/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class PageRedirection extends StatelessWidget {
+  final String message;
+  final String option;
+
+  const PageRedirection(
+      {super.key,
+      this.message = "¿No tienes cuenta aun?",
+      this.option = "Resgistrate"});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(message, style: TextStyle(color: Colors.white, fontSize: 17)),
+          TextButton(
+              onPressed: () {},
+              child: Text(option,
+                  style: TextStyle(
+                      color: AppColors.primaryColor,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold)))
+        ],
+      ),
+    );
+  }
+}
